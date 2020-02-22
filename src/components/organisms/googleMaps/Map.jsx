0ecb8f4ae8e4ed5b42eaef7deb1897f2cf87ea88
Jouler Pragma <jouler.talaigua@pragma.com.co>
2020-02-21 19:02:49 -0500

    login view

--- src/components/organisms/googleMaps/Map.jsx
+++ src/components/organisms/googleMaps/Map.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps'
 import './Map.scss'
 
 
 const Map = ({ salesPoints, onClickMarker, activePoint }) => {
 
-    const [labelValue, setLabelValue] = useState('')
+    // const [labelValue, setLabelValue] = useState('')
 
     return (
         <GoogleMap zoom={activePoint.zoom ? activePoint.zoom : 7} center={{ lat: activePoint.latitude, lng: activePoint.longitude }}>
--- src/pages/singIn.jsx
+++ src/pages/singIn.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import SinIngForm from "../components/organisms/SinIngForm/SinIngForm";
-import Layout from "../components/organisms/layout/layout";
 
 const singIn = () => {
   return (
-    <Layout isLoguerUser={true}>
+    <>
       <SinIngForm />
-    </Layout>
+    </>
   );
 };
 
